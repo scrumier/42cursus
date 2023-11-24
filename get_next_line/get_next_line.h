46d4f68bc7e66db2ce03a6f47712472e3fba992e
Sonam Crumiere <sonamcrumiere@Sonams-MacBook-Air.local>
2023-11-24 11:22:07 +0100

    g

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -5,13 +5,18 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: sonamcrumiere <sonamcrumiere@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/20 18:36:17 by sonamcrumie       #+#    #+#             */
-/*   Updated: 2023/11/21 10:26:21 by sonamcrumie      ###   ########.fr       */
+/*   Created: 2023/11/23 11:22:35 by sonamcrumie       #+#    #+#             */
+/*   Updated: 2023/11/23 11:22:38 by sonamcrumie      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include <unistd.h>
-#define BUFFER_SIZE 42
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# define BUFFER_SIZE 42
 
 char	*get_next_line(int fd);
+
+#endif
