ff31078fa1cb5bfd8d3e430625476a82d8b25616
Sonam Crumiere <sonamcrumiere@Sonams-MacBook-Air.local>
2023-12-08 12:52:09 +0100

    ajout so_long

---
+++ so_long/so_long.h
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sonamcrumiere <sonamcrumiere@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/06 11:24:08 by sonamcrumie       #+#    #+#             */
+/*   Updated: 2023/12/06 11:26:55 by sonamcrumie      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SOLONG_H
+# define SOLONG_H
+# include <math.h>
+# include <stdlib.h>
+# include <unistd.h>
+#endif
