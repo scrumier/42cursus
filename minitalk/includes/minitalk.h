711a1966487d71b22230f604450bf9c5c26c7c72
Sonam Crumiere <sonamcrumiere@Sonams-MacBook-Air.local>
2024-01-30 09:20:41 +0100

    ajout de minitalk

---
+++ minitalk/includes/minitalk.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sonamcrumiere <sonamcrumiere@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/29 13:52:46 by sonamcrumie       #+#    #+#             */
+/*   Updated: 2024/01/29 15:44:29 by sonamcrumie      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include "../libft/libft_src/libft.h"
+# include "../ft_printf/ft_printf.h"
+# include <signal.h>
+
+#endif
---
+++ minitalk/src/client.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sonamcrumiere <sonamcrumiere@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/29 15:42:57 by sonamcrumie       #+#    #+#             */
+/*   Updated: 2024/01/29 15:43:55 by sonamcrumie      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minitalk.h"
+
+void	ft_send_bits(int pid, char i)
+{
+	int	bit;
+
+	bit = 0;
+	while (bit < 8)
+	{
+		if ((i & (0x01 << bit)) != 0)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		usleep(100);
+		bit++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	pid;
+	int	i;
+
+	i = 0;
+	if (argc == 3)
+	{
+		pid = ft_atoi(argv[1]);
+		while (argv[2][i] != '\0')
+		{
+			ft_send_bits(pid, argv[2][i]);
+			i++;
+		}
+		ft_send_bits(pid, '\n');
+	}
+	else
+	{
+		return (1);
+	}
+	return (0);
+}
